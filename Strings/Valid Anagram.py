637a3a953ded3c034f5c70d0ac5f347ade928cf9
shreya <90253984+shreyasrestha@users.noreply.github.com>
2026-06-28 19:35:02 +0530

    Implement valid anagram checker with two approaches
    Added two approaches for checking valid anagrams using sorting and dictionaries.

--- Strings/Valid Anagram.py
+++ Strings/Valid Anagram.py
@@ -1,1 +1,27 @@
+#Problem: Valid Anagram
+#Platform: LeetCode
+#Difficulty: Easy
+#Topic: String
+
+#Approach 1 (By using the sorted function)
+#Time complexity = O(nlogn)
+
+def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s)==sorted(t)
+  
+#Approach 2 (By using Dictionary)
+#Time Complexity= O(n)
+
+def isAnagram(self, s: str, t: str) -> bool:
+        s_d={}
+        t_d={}
+
+        if len(s)!=len(t):
+            return False
+
+        for i in range(len(s)):
+            s_d[s[i]] = s_d.get(s[i], 0) + 1
+            t_d[t[i]] = t_d.get(t[i], 0) + 1
+
+        return s_d==t_d
 
