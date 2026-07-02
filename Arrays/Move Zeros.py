5aca15070c0697fced83047f9aab1d27a7fc6022
shreya <90253984+shreyasrestha@users.noreply.github.com>
2026-07-02 12:44:24 +0530

    Create Move Zeros.py

---
+++ Arrays/Move Zeros.py
@@ -0,0 +1,17 @@
+#Problem: Move Zeros
+#Platform: LeetCode
+#Difficulty: Easy
+#Topic: Arrays
+
+#Time Complexity=O(n)
+
+def moveZeroes(self, nums: List[int]) -> None:
+        n=len(nums)
+        j=0
+        for i in range(n):
+            if nums[i]!=0:
+                #nums[i],nums[j]=nums[j],nums[i]
+                temp=nums[j]
+                nums[j]=nums[i]
+                nums[i]=temp
+                j+=1
